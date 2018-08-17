7c7309e0fce37940b2a57123ebf63f155ad0353f
Matthew Campbell <cattmampbell@gmail.com>
2018-08-16 23:02:55 -0400

    Added increased functionality to page via app.js file.

--- app.js
+++ app.js
@@ -19,11 +19,24 @@ let meMatthew = {
 // An array of my interests.
 let myInterests = ["Music", "Space & Science", "Woodworking", "Arduino & Raspberry Pi", "My lovely wife, Olivia and our cat, Toast"];
 
+// An array of my previous experience.
+let myExperience = [
+    "",
+    "",
+    ""
+];
+
 console.log("Name: " + meMatthew.name); // Should print: "Name: Matthew Campbell".
 console.log("Career: " + meMatthew.career); // Should print: "Career: Full Stack Engineer / Independent Contractor at Amazon".
-console.log("Description: " + meMatthew.career);
+console.log("Description: " + meMatthew.description); // Should print: "Description: I love pizza."
 
 console.log("My Interests: "); 
-for(let i = 0; i < myInterests.length[i]; i++) {
-    console.log(myInterests[i]); // Should print each item in 'myInterests'.
-}
+for(let i = 0; i < myInterests.length; i++) {
+    console.log("*   " + myInterests[i]); // Should print each item in 'myInterests'.
+};
+
+console.log("My Previous Experience: ");
+for(let i = 0; i < myExperience.length; i++) {
+    console.log("*  " + myExperience[i]); // Should print each item in 'myExperience'.
+};
+
