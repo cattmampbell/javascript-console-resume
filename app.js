9f94d86c4512b030ef23aea1a4f9b2eeed75ecc8
Matthew Campbell <cattmampbell@gmail.com>
2018-08-17 22:05:31 -0400

    Added multiple functions to app.js file.

--- app.js
+++ app.js
@@ -1,42 +1,66 @@
-let myFirstName = "Matthew";
-let myMiddleName = "Allen";
-let myLastName = "Campbell";
-let myFullName = myFirstName + " " + myLastName; // String concatenation of 'myFirstName' and 'myLastName'.
+let firstName = "Matthew";
+let middleName = "Allen";
+let lastName = "Campbell";
+let fullName = firstName + " " + middleName + " " + lastName;
 
-// Variables ('myCareerA' and 'myCareerB') that make up career. 
-let myCareerA = "(Aspiring) Full Stack Engineer";
-let myCareerB = "Independent Contractor at Amazon";
+// Calls displayDescription() Function
+displayDescription(fullName, "(Aspiring) Full Stack Developer / Photo Studio Associate", "I like pizza");
 
-let myDescription = "I love pizza.";
+// Function that accepts x3 arguments (Name, Career, Description), formats accordingly and prints them to the Console. 
+function displayDescription(myName, myCareer, myDescription) {
+    console.log("Name: " + myName);
+    console.log("Career: " + myCareer);
+    console.log("Description: " + myDescription + ".");
+}
 
-// An object ('meMatthew') of my basic info.
-let meMatthew = {
-    name: myFullName,
-    career: myCareerA + " / " + myCareerB,
-    description: myDescription
+// My Interests:
+console.log("My Interests: ");
+// Calls displayInterests() Function
+displayInterests("Music"); // 1) Should print - Music
+displayInterests("Space & Science"); // 2) Should print - Space & Science
+displayInterests("Making Things & Woodworking"); // 3) Should print - Making Things & Woodworking
+displayInterests("Arduino & Raspberry Pi"); // 4) Should print - Arduino & Raspberry Pi
+displayInterests("Spending time with my lovely wife, Olivia and our cat, Toast"); // 5) Should print - Spending time with my lovely wife, Olivia and our cat, Toast
+
+// Function that accepts x1 argument (Interest), formats accordingly and prints to the Console.
+function displayInterests(myInterest) {
+    console.log("*  " + myInterest); 
+}
+
+// My Previous Experiences:
+console.log("My Previous Experiences: ");
+// Calls the displayPosition() Function
+// 969 Creative
+displayPosition("969 Creative", "Photo Studio Associate", "Provided quality images for Amazon.com by following standards and procedures of the Style Guide");
+// Amazon
+displayPosition("Amazon", "Fulfillment Center Associate", "Ensured all incoming product / inventory was stored correctly in the appropriate locations according to Standard Work");
+// Radco Residential (Radius Mountain Creek)
+displayPosition("Radco Residential (Radius Mountain Creek)", "Maintenance Technician", "Guaranteed excellent living conditions for all tenants by performing necessary repairs");
+
+// Function that accepts x3 arguments (Company Name, Job Title and Job Description), formats accordingly and prints them to the Console. 
+function displayPosition(companyName, jobTitle, jobDescription) {
+    console.log("*  " + companyName + ", " + jobTitle + " - " + jobDescription + ". ");
 };
 
-// An array of my interests.
-let myInterests = ["Music", "Space & Science", "Woodworking", "Arduino & Raspberry Pi", "My lovely wife, Olivia and our cat, Toast"];
+// My Skills:
+console.log("My Skills: ");
+// Calls dispaySkill() Function
+displaySkill("Playing Drums", true); // 1) Should print - BAM: Playing Drums
+displaySkill("HTML5", false); // 2) Should print - HTML5
+displaySkill("CSS3", false); // 3) Should print - CSS3
+displaySkill("Bootstrap 4.0", true); // 4) Should print - BAM: Bootstrap 4.0
+displaySkill("JavaScript", true); // 5) Should print - BAM: JavaScript
+displaySkill("Soldering", true); // 6) Should print - BAM: Soldering
+displaySkill("Arduino Programming", true); // 7) Should print - BAM: Arduino Programming
+displaySkill("Markdown", false); // 8) Should print - Markdown
+displaySkill("Git / GitHub", false); // 9) Should print - Git / GitHub
+displaySkill("Audio Engineering", true); // 10) Should print - BAM: Audio Engineering
 
-// An array of my previous experience.
-let myExperience = [
-    "",
-    "",
-    ""
-];
-
-console.log("Name: " + meMatthew.name); // Should print: "Name: Matthew Campbell".
-console.log("Career: " + meMatthew.career); // Should print: "Career: Full Stack Engineer / Independent Contractor at Amazon".
-console.log("Description: " + meMatthew.description); // Should print: "Description: I love pizza."
-
-console.log("My Interests: "); 
-for(let i = 0; i < myInterests.length; i++) {
-    console.log("*   " + myInterests[i]); // Should print each item in 'myInterests'.
-};
-
-console.log("My Previous Experience: ");
-for(let i = 0; i < myExperience.length; i++) {
-    console.log("*  " + myExperience[i]); // Should print each item in 'myExperience'.
-};
-
+// Function that accepts x2 arguments (Skill Name / Boolean Value), formats accordingly and prints them to the Console. 
+function displaySkill(mySkill, bamBam) {
+    if(bamBam == true) { // If (bamBam) true ...BAM: and 'mySkill'.
+        console.log("*  BAM: " + mySkill);
+    } else if(bamBam == false) {
+        console.log("*  " + mySkill); // Else If (bamBam) false ...just 'mySkill'.
+    };
+}
